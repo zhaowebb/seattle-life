f6b1b1a04b9126d36003fcdd4cd16317f3ffe00a
zhaowebb <zhaowebb@gmail.com>
2017-11-29 19:48:31 -0500

    modify header

--- server/index.js
+++ server/index.js
@@ -130,7 +130,7 @@ app.get('/:lat/:lng/:floorplan/:isRent', async (req, res) => {
 	let utilities = await findUtilities(lat, lng)
 	let crimes = await findCrimes(lat, lng)
 
-	res.setHeader('Access-Control-Allow-Origin', 'https://seattle-life.herokuapp.com');
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);

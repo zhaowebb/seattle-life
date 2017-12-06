83c09d10322ba9e5c5a12c258cf1d13ec03d66cb
zhaowebb <zhaowebb@gmail.com>
2017-12-06 15:43:58 -0500

    get new restaurant data

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ const deepcopy = require("deepcopy")
 const findRestaurants = (lat, lng) => {
 	return new Promise((resolve, reject) => {
 		const restaurants = Restaurant.find({
-		// lat: {$gt: lat - 2, $lt: lat + 2},
-		// lng: {$gt: lng - 2, $lt: lng + 2},
-		lat: {$gt: 33.6 - 2, $lt: 33.6 + 2},
-		lng: {$gt: -111.9 - 2, $lt: -111.9 + 2},
+		lat: {$gt: lat - 0.05, $lt: lat + 0.05},
+		lng: {$gt: lng - 0.05, $lt: lng + 0.05},
+		// lat: {$gt: 33.6 - 2, $lt: 33.6 + 2},
+		// lng: {$gt: -111.9 - 2, $lt: -111.9 + 2},
 
 		category: {$in: ["Restaurants"]}
 	})
